c9b1c4a073cec2786caaae63391134f8492a4149
Yaroslav Shirokov <slava@github.com>
2013-02-24 17:43:00 -0800

    JS2

--- init.d/02-require.el
+++ init.d/02-require.el
@@ -1,6 +1,15 @@
+;; Features
 (require 'smex)
 (require 'dedicated)
-(require 'coffee-mode)
 (require 'ssh)
+
+
+;; IRC and IRC Accessories
+(require 'erc-nick-colors)
 (require 'znc)
 (require 'tls)
+
+;; Edit modes
+(require 'coffee-mode)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
